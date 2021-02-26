f86bc2d784b8674a0826b1cc4f746d816d0b2c29
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2021-02-26 14:15:05 +0700

    Client: Routes and Pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
-const App = () => (
-
-  <div>
-    <p>react app</p>
-  </div>
+const App = () => {
   
-);
+  return (
+    <Switch>
+
+        <Route exact path="/" component={Home}></Route>
+
+        <Route exact path="/register" component={Register}></Route>
+
+        <Route exact path="/login" component={Login}></Route>
+
+    </Switch>
+  );
+
+}
 
 export default App;
