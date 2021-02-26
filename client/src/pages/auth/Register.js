d52d1f1c6f071c64c601e0e62e3e19b060c0a406
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2021-02-27 00:05:31 +0700

    Client: Email Link to Signin

--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,17 +1,45 @@
 import React, { useState } from 'react';
+import { auth } from '../../firebase';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
 
     const [email, setEmail] = useState('');
 
-    const handleSubmit = () => {
-
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const actionCodeSettings = {
+            // URL you want to redirect back to. The domain (www.example.com) for this
+            // URL must be in the authorized domains list in the Firebase Console.
+            url: 'http://localhost:3000/register/complete',
+            // This must be true.
+            handleCodeInApp: true,
+        };
+        
+        await auth.sendSignInLinkToEmail(email, actionCodeSettings);
+        toast.success(`Email is sent to ${email}. Click the link to complete your registration`);
+        
+        window.localStorage.setItem('emailForSignIn', email);
+        setEmail('');
     }
 
     const registerForm = () => (
         <form onSubmit={handleSubmit}>
-            <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" autoFocus />
-            <button type="submit" className="btn btn-raised">Register / {email}</button>
+            <input 
+                type="email" 
+                className="form-control" 
+                value={email} 
+                onChange={(e) => setEmail(e.target.value)} 
+                placeholder="Enter your email" 
+                autoFocus />
+
+            <button 
+                type="submit" 
+                className="btn btn-raised">
+                Register / {email}
+            </button>
         </form>
     );
     
@@ -23,7 +51,9 @@ const Register = () => {
                     {registerForm()}
                 </div>
             </div>
+            <ToastContainer />
         </div>
+        
     );
 
 }
