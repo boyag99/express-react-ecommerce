447f63252fa832eae3f6d27e253c2e29c7c957c3
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2021-03-01 12:50:55 +0700

    Server: User Schema

---
+++ server/src/models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const { ObjectId } = Schema;
+
+const userSchema = new Schema({
+    name: String,
+    email: {
+        type: String,
+        required: true,
+        index: true
+    },
+    role: {
+        type: String,
+        default: 'subscriber'
+    },
+    cart: {
+        type: Array,
+        default: []
+    },
+    address: String,
+    // wishlist: [{
+    //     type: ObjectId,
+    //     ref: 'Product'
+    // }]
+}, { timestamp: true });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
