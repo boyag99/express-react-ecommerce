725a12dfaf064cbcd265fc0af8f2984752d329f9
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2021-03-01 13:14:26 +0700

    Server: Middleware

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {createOrUpdateUser} = require('../controllers/auth');
+const { authCheck } = require('../middlewares/auth');
 
-router.get('/create-or-update-user', createOrUpdateUser);
+router.get('/create-or-update-user', authCheck, createOrUpdateUser);
 
 module.exports = router;
