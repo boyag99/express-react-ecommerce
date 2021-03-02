9a2d2edfd754c88a3dd99be20331e0ca3ac04aca
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2021-03-03 01:54:13 +0700

    Client: Category CRUD Requests

---
+++ client/src/functions/category.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+
+const CREATE = 'category/create';
+const READ = 'category/read';
+const UPDATE = 'category/update';
+const REMOVE = 'category/remove';
+const LIST = 'category/categories';
+
+export const createCategory = async (token, name) => {
+    return await axios.post(`${process.env.REACT_APP_API}/${CREATE}`, { name }, {
+        headers: {
+            token
+        }
+    });
+}
+
+export const getCategory = async (token, slug) => {
+    return await axios.get(`${process.env.REACT_APP_APP}/${READ}/${slug}`, {
+        headers: {
+            token
+        }
+    });
+}
+
+export const updateCategory = async (token, name, slug) => {
+    return await axios.put(`${process.env.REACT_APP_API}/${UPDATE}/${slug}`, { name }, {
+        headers: {
+            token
+        }
+    });
+}
+
+export const removeCategory = async (token, slug) => {
+    return await axios.delete(`${process.env.REACT_APP_API}/${REMOVE}/${slug}`, {
+        headers: {
+            token
+        }
+    });
+}
+
+export const getCategories = async () => {
+    return await axios.get(`${process.env.REACT_APP_API}/${LIST}`);
+}
