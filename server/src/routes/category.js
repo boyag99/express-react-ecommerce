e7f1ad5ef8c91b6b67aa10bdf67855da8a5ca198
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2021-03-03 00:37:45 +0700

    Server: Category Routes & Controllers

---
+++ server/src/routes/category.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { authCheck, adminCheck } = require('../middlewares/auth');
+const { create, read, update, remove, list } = require('../controllers/category');
+
+router.post('/category/create', authCheck, adminCheck, create);
+router.get('/category/read/:slug', authCheck, adminCheck, read);
+router.put('/category/update/:slug', authCheck, adminCheck, update);
+router.delete('/category/remove/:slug', authCheck, adminCheck, remove);
+router.get('/categories', authCheck, adminCheck, list);
+
+module.exports = router;
