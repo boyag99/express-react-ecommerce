24de9bb9ef651834a233685bf87597ba23f6bf2f
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2021-03-03 01:45:07 +0700

    Server: Category List Read Update Delete

--- server/src/controllers/category.js
+++ server/src/controllers/category.js
@@ -6,7 +6,7 @@ exports.create = async (req, res) => {
     const slug = slugify(name);
 
     try {
-        const newCategory = await new Category({ 
+        const newCategory = await Category({ 
             name, 
             slug 
         }).save();
@@ -17,18 +17,75 @@ exports.create = async (req, res) => {
     }
 }
 
-exports.read = (req, res) => {
+exports.read = async (req, res) => {
+    const { slug } = req.params;
+
+    try {
+        const category = await Category.findOne({ slug }).exec();
+
+        if (category) {
+            res.status(200).json(category);
+        } else {
+            res.status(404).json({
+                message: `The category could not be found in database.`
+            });
+        }
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+exports.update = async (req, res) => {
+    const { slug } = req.params;
+    const { name } = req.body;
+    const newSlug = slugify(name);
+
+    try {
+        const category = await Category.findOneAndUpdate({ slug }, { name, slug: newSlug }, { new: true });
+
+        if (category) {
+            res.status(200).json(category);
+        } else {
+            res.status(404).json({
+                message: `The category could not be found in database.`
+            });
+        }
+    } catch (error) {
+        res.status(400).json(error);
+    }
 
 }
 
-exports.update = (req, res) => {
+exports.remove = async (req, res) => {
+    const { slug } = req.params;
+    
+    try {
+        const category = await Category.findOneAndDelete({ slug });
 
+        if (category) {
+            res.status(200).json(category);
+        } else {
+            res.status(404).json({
+                message: `The category could not be found in database.`
+            });
+        }
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
 
-exports.remove = (req, res) => {
+exports.list = async (req, res) => {
+    try {
+        const categories = await Category.find({}).sort({ createAt: -1 });
 
-}
-
-exports.list = (req, res) => {
-
+        if (categories) {
+            res.status(200).json(categories);
+        } else {
+            res.status(404).json({
+                message: `The categories could not be found in database.`
+            });
+        }
+    } catch (error) {
+        res.status(400).json(error);
+    }
 }
