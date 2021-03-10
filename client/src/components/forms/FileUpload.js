8ae44a9c7b08263d8d2a44e8bb6ac660d7384902
Trinh Hao Hiep <trinhhaohiep26071999@gmail.com>
2021-03-10 21:50:58 +0700

    File Resize in React

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import Resizer from 'react-image-file-resizer';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const FileUpload = () => {
     const fileUploadAndResize = (e) => {
-        console.log(e.target.files);
+        let files = e.target.files;
+
+        if (files) {
+            for (let i = 0; i < files.length; i++) {
+                Resizer.imageFileResizer(
+                    files[i],
+                    720,
+                    720,
+                    'JPEG',
+                    100,
+                    0,
+                    (uri) => {
+                        console.log(uri);
+                    },
+                    'base64'
+                );
+            }
+        }
     };
     return (
         <div className='form-group'>
